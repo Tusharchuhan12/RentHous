bec060210f7f2e1d1860fdc6dcd79da1d663f239
tushar chauhan <tusharchauhan120104@gmail.com>
2025-09-06 03:07:19 +0530

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
+
+import { FaWhatsapp } from "react-icons/fa";
+
 import Login from "./page/Login";
 import Signup from "./page/Signup";
 import Home from "./page/Home";
@@ -9,13 +13,24 @@ import Buy from "./page/Buy";
 function App() {
   const location = useLocation();
 
- 
-  const hideLayout = location.pathname === "/login" || location.pathname === "/signup";
+  // Hide Navbar + Footer on login & signup pages
+  const hideLayout =
+    location.pathname === "/login" || location.pathname === "/signup";
+
+  // WhatsApp floating button
+  const phone = "918630116843";
+  const message = "Hello! I need more details.";
+  const href = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+  const position = "bottom-20 right-5";
 
   return (
     <>
-      {!hideLayout && <Navbar />} 
+   
 
+   
+      {!hideLayout && <Navbar />}
+
+      {/* Routes */}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
@@ -23,9 +38,21 @@ function App() {
         <Route path="/buy" element={<Buy />} />
       </Routes>
 
+    
       {!hideLayout && <Footer />}
+
+    
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`fixed ${position} w-14 h-14 bg-green-600 rounded-full flex items-center justify-center shadow-lg hover:bg-green-700 transition`}
+      >
+        <FaWhatsapp className="w-8 h-8 text-white" />
+      </a>
     </>
   );
 }
 
 export default App;
+``
